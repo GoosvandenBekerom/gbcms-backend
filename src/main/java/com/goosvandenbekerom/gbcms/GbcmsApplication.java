85bd40a2144a9a3115ad20f50e11868407ae7abc
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-08-20 11:25:39 +0200

    initial commit

---
+++ src/main/java/com/goosvandenbekerom/gbcms/GbcmsApplication.java
@@ -0,0 +1,12 @@
+package com.goosvandenbekerom.gbcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbcmsApplication.class, args);
+    }
+}
