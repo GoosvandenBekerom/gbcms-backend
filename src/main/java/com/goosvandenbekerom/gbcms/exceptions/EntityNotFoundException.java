0183551a81cb037d1777f8eff5a5dec5ec6a0033
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-08-20 16:24:12 +0200

    changed to runtime exception

--- src/main/java/com/goosvandenbekerom/gbcms/exceptions/EntityNotFoundException.java
+++ src/main/java/com/goosvandenbekerom/gbcms/exceptions/EntityNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntityNotFoundException extends Exception {
+public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(Class entityType, Object identifier) {
         super(String.format("%s with id %s was not found", entityType.getSimpleName(), identifier));
     }
