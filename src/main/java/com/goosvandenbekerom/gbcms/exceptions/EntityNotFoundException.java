53740677001e1bc7014dbacadd107cd56009812a
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-08-20 16:20:50 +0200

    removed typo

--- src/main/java/com/goosvandenbekerom/gbcms/exceptions/EntityNotFoundException.java
+++ src/main/java/com/goosvandenbekerom/gbcms/exceptions/EntityNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends Exception {
     public EntityNotFoundException(Class entityType, Object identifier) {
-        super(String.format("%s with id $s was not found", entityType.getSimpleName(), identifier));
+        super(String.format("%s with id %s was not found", entityType.getSimpleName(), identifier));
     }
 }
